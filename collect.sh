6c23d5e232d1f08ad85eee9a83841dcdc518806c
coolrc136 <renchao13619102485@gmail.com>
2015-12-07 22:57:11 +0000

    新建配置

---
+++ collect.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+######################################################
+#bash配置
+cp ~/.bashrc ./bash/
+#vim配置
+cp /etc/vimrc ./vim/
+#pacman配置
+cp /etc/pacman.d/mirrorlist ./pacman/
+cp /etc/pacman.conf ./pacman/
---
+++ install.sh
@@ -0,0 +1,4 @@
+cp ./bash/bashrc ~/.bashrc
+sudo cp ./vim/vimrc /etc/vimrc
+sudo cp ./pacman/mirrorlist /etc/pacman.d/mirrorlist
+sudo cp ./pacman/pacman.conf /etc/pacman.conf
