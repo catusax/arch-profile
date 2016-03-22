e4d5af7207861fcfbb7be1e3f02eede8ba79e760
coolrc136 <renchao13619102485@gmail.com>
2016-03-22 12:31:58 +0800

    update

--- bingimg/bingimg.sh
+++ bingimg/bingimg.sh
@@ -12,12 +12,14 @@ fi
 if [ ! -f ~/图片/bingimg/${mouth}/${date}***.jpg ];then
     wget -t 10 -O ~/.bingimg "http://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1"
     imgurl=`grep -o http.*jpg ~/.bingimg`
-    text=`grep -o -P "(?<=copyright\":\").*(?=\ （©)" ~/.bingimg`
+    text1=`grep -o -P "(?<=copyright\":\").*(?=\ \(©)" ~/.bingimg`
+    text=`echo $text1 | sed s/[[:space:]]//g`
     wget -t 10 -O  ~/图片/bingimg/${mouth}/${date}-${text}.jpg "${imgurl}"
 
     gsettings set org.gnome.desktop.background picture-uri ~/图片/bingimg/${mouth}/${date}-${text}.jpg
 
 fi
 echo $text
+echo $text1
 sleep 20 
 done
